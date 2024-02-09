59b647a5c1f9d845efd6c29a0029edd8f1287000
Alexandre <alexandreaddres@gmail.com>
2024-02-09 04:03:33 -0300

    adding delete function with success

--- src/app.tsx
+++ src/app.tsx
@@ -29,12 +29,22 @@ export function App() {
       date: new Date(),
       content,
     }
-
+  
     const notesArray = [newNote, ...notes]
 
     setNotes(notesArray) // copy all notes that i have, and add a new note
 
     localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function whenNoteDeleted(id: string){
+    const newArrayWithoutThisIDNote = notes.filter(note => {
+      return note.id != id
+    })
+
+    setNotes(newArrayWithoutThisIDNote)
+
+    localStorage.setItem('notes', JSON.stringify(newArrayWithoutThisIDNote))
   }
 
   function handleUserSearch(userSearchInformation: ChangeEvent<HTMLInputElement>){
@@ -49,18 +59,17 @@ export function App() {
 
   return (
     <>
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="Only react logo" />
       <form action="w-full">
-        <input type="text" name="" id="" placeholder="I want the note..." className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 border-green-500" onChange={handleUserSearch}/>
+        <input type="text" placeholder="I want the note..." className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 border-green-500" onChange={handleUserSearch}/>
       </form>
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewCardNote whenNoteCreated={whenNoteCreated}/>
-        {/* <CardNote new_note_information={note_information}/> */}
 
         {filtredNotes.map(note => {
-          return <CardNote key={note.id} new_note_information={note}/>
+          return <CardNote key={note.id} new_note_information={note} whenDeleteNote={whenNoteDeleted}/>
         })}
 
       </div>
